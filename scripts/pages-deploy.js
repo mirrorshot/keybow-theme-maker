17acc2f25b8b125a5695e2f8b1837085e676c337
mirrorshot <brambilla.daniele@outlook.com>
2022-06-05 15:20:05 +0200

    Fix for deploy procedure

--- scripts/pages-deploy.js
+++ scripts/pages-deploy.js
@@ -5,6 +5,7 @@ const fs = require("fs");
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         console.log("Building...");
+        await execa("npm", ["install"]);
         await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
